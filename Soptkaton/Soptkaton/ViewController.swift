fedb9dc1c4c10197a69f0f35d789ceeea2b842af
hooni <jhlee909090@gmail.com>
2024-11-21 22:07:00 +0900

    setting: 기본세팅 완료

---
+++ Soptkaton/Soptkaton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Soptkaton
+//
+//  Created by 이지훈 on 11/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
