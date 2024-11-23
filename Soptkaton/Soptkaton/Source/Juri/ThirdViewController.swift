40edbaaeca614415b8f2382b4103f5f678f17e3c
JURI <80569323+juri123123@users.noreply.github.com>
2024-11-24 01:12:07 +0900

    feat-#17 custom alert view controller
    ThirdViewController에서 임시로 버튼 눌러서 alert 호출하고 있습니다!!!!!!!!!

--- Soptkaton/Soptkaton/Source/Juri/ThirdViewController.swift
+++ Soptkaton/Soptkaton/Source/Juri/ThirdViewController.swift
@@ -16,6 +16,12 @@ class ThirdViewController: UIViewController {
         $0.text = "안녕하시렵니까? 스토리텔링 화면입니다만.. 무얼 더 해야하죠...."
         $0.numberOfLines = 0
     }
+    
+    //임시
+    private lazy var button = UIButton().then {
+        $0.setTitle("alert !!! ", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +29,22 @@ class ThirdViewController: UIViewController {
         setHierarchy()
         setLayout()
         
+        //임시
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+    }
+    
+    // 임시
+    @objc
+    private func buttonTapped() {
+        let viewController = CustomAlertViewController()
+        viewController.experienceText = 40
+        viewController.modalPresentationStyle = .overFullScreen
+        present(viewController, animated: true)
     }
     
     private func setHierarchy() {
-        view.addSubview(storyLabel)
+        view.addSubviews(storyLabel, button)
     }
     
     private func setLayout() {
@@ -36,6 +54,11 @@ class ThirdViewController: UIViewController {
             $0.edges.equalToSuperview().inset(10)
         }
         
+        // 임시
+        button.snp.makeConstraints {
+            $0.bottom.equalTo(safeArea.snp.bottom).inset(100)
+            $0.centerX.equalToSuperview()
+        }
     }
 
 }
