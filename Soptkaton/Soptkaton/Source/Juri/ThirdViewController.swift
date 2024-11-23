469ccdb62c718be15d78be166c0e4385205026d5
JURI <80569323+juri123123@users.noreply.github.com>
2024-11-24 01:21:40 +0900

    del-#17 필요없는 코드 삭제

--- Soptkaton/Soptkaton/Source/Juri/ThirdViewController.swift
+++ Soptkaton/Soptkaton/Source/Juri/ThirdViewController.swift
@@ -16,12 +16,6 @@ class ThirdViewController: UIViewController {
         $0.text = "안녕하시렵니까? 스토리텔링 화면입니다만.. 무얼 더 해야하죠...."
         $0.numberOfLines = 0
     }
-    
-    //임시
-    private lazy var button = UIButton().then {
-        $0.setTitle("alert !!! ", for: .normal)
-        $0.setTitleColor(.black, for: .normal)
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,22 +23,10 @@ class ThirdViewController: UIViewController {
         setHierarchy()
         setLayout()
         
-        //임시
-        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-        
-    }
-    
-    // 임시
-    @objc
-    private func buttonTapped() {
-        let viewController = CustomAlertViewController()
-        viewController.experienceText = 40
-        viewController.modalPresentationStyle = .overFullScreen
-        present(viewController, animated: true)
     }
     
     private func setHierarchy() {
-        view.addSubviews(storyLabel, button)
+        view.addSubviews(storyLabel)
     }
     
     private func setLayout() {
@@ -53,12 +35,7 @@ class ThirdViewController: UIViewController {
         storyLabel.snp.makeConstraints {
             $0.edges.equalToSuperview().inset(10)
         }
-        
-        // 임시
-        button.snp.makeConstraints {
-            $0.bottom.equalTo(safeArea.snp.bottom).inset(100)
-            $0.centerX.equalToSuperview()
-        }
+    
     }
 
 }
